ae09d8d22adbf9d8fc37813eb76496aab40454b0
Maxime HEBRARD <maxime.hebrard@gmail.com>
2017-09-08 12:18:08 +0900

    Hello World

---
+++ main.js
@@ -0,0 +1,43 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+// Global reference of main window
+let win;
+
+function createWindow() {
+  // Create the browser window
+  win = new BrowserWindow();
+  // Load index.html in the window
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  // Delete the window object when close
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+// Run the app
+app.on('ready', createWindow);
+
+// Quit the app when all windows are closed
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// Re-create window
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+});
